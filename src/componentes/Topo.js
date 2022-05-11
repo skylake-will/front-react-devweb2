8a053200102a65809979da475de35606d6b59580
Williams Araujo <williams.s001.ws@gmail.com>
2022-05-11 10:32:13 -0300

    Alterações feitas no css

--- src/componentes/Topo.js
+++ src/componentes/Topo.js
@@ -4,61 +4,48 @@ import './Topo.css';
 import { GiAtomicSlashes } from "react-icons/gi";
 import { CgHomeAlt, CgProfile, CgBookmark, CgSearch } from "react-icons/cg";
 
-export default function Topo(){
-    return(
+export default function Topo() {
+    return (
         <>
-        <header className="header">
-            <div className="menu-principal"/>
-                {/* <div className="logo"/>
-                <div/>  */}
-                <div className="title-logo"> 
-                    <a href=''><GiAtomicSlashes/></a>
+            <header>
+                <section className="container">
+                <div className="title-logo">
+                    <a href=''><GiAtomicSlashes /></a>
                     <a href=''>namelessproject</a>
-                     
                 </div>
 
-        
-        <main className="menu-urls">
-            <div className="header2">
-                <div className="menu">
-                    <ul>
-                        <li>
-                            <a href=""><CgHomeAlt/></a>
-                        </li>
-    
-                        <li>
-                            <a href=""><CgProfile/></a>
-                        </li>
-    
-                        <li>
-                            <a href=""><CgBookmark/></a>
-                        </li>
+                <section className="menu-urls">
+                    <div className="menu">
+                        <ul>
+                            <li>
+                                <a href=""><CgHomeAlt /></a>
+                            </li>
 
-                        <li>
-                            <a href=""></a>
-                        </li>
-                    </ul>
-                    <form action="/" method="get">
-                        <label htmlFor="header-search">
-                            <span className="busca">Pesquisar</span>
-                        </label>
-                        <input
-                            type="text"
-                            id="header-search"
-                            placeholder="Pesquisar"
-                            name="s" 
-                        />
-                        <button type="submit"><CgSearch/></button>
-                    </form>
-                </div>
-            </div>
-        </main>
-        
-        </header>
+                            <li>
+                                <a href=""><CgProfile /></a>
+                            </li>
+
+                            <li>
+                                <a href=""><CgBookmark /></a>
+                            </li>
+
+                            <li>
+                                <a href=""></a>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <form action="/" method="get">
+                    <input
+                        type="text"
+                        id="header-search"
+                        placeholder="Pesquisar"
+                        name="s"
+                    />
+                    <button type="submit"><CgSearch/></button>
+                </form>
+                </section>
+            </header>
         </>
-
-        
-
-        
     )
 }
